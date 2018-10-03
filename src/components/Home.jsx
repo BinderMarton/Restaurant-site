20a4f242098d7dfb954dc12ae2cb27018c4ec1e8
BinderMarton <marci.binder01@gmail.com>
2018-10-04 00:07:48 +0200

    last codes2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,21 +11,14 @@ import {SideData} from "./Languages"
 
 
 class Home extends Component {
-    state = {
-        pageHelp: true
-    }
-
-    componentDidMount() {
-        this.setState({ pageHelp: false })
-    }
 
     render() {
         return (
             <div className="home">
                 <div className="home-container">
-                    <Animated animationIn="bounceInLeft" isVisible={this.state.pageHelp}><h1 className="restaurant-name">{SideData[this.props.language].Home.name}</h1> </Animated>
-                    <Animated animationIn="bounceInRight" isVisible={this.state.pageHelp}><p className="name2">{SideData[this.props.language].Home.text2}</p></Animated>
-                    <Animated className="separator" animationIn="bounceInUp" isVisible={this.state.pageHelp}></Animated>
+                    <Animated animationIn="bounceInLeft" isVisible={true}><h1 className="restaurant-name">{SideData[this.props.language].Home.name}</h1> </Animated>
+                    <Animated animationIn="bounceInRight" isVisible={true}><p className="name2">{SideData[this.props.language].Home.text2}</p></Animated>
+                    <Animated className="separator" animationIn="bounceInUp" isVisible={true}></Animated>
                 </div>
                 <Footer changeLanguage={this.props.changeLanguage}/>
             </div>
