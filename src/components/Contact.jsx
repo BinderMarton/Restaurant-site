8289e395741f1451e6d2b092953fc9f50425ca12
BinderMarton <marci.binder01@gmail.com>
2018-10-03 20:28:40 +0200

    facebook page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet'
 import './Contact.css';
-import {SideData} from "./Languages"
+import { SideData } from "./Languages"
+import FacebookProvider, { Page } from 'react-facebook';
 
 
 class Contact extends Component {
@@ -19,23 +20,26 @@ class Contact extends Component {
         return (
             <div className="width-container">
                 <div className="contact-container">
-               <div className="contact-data">
+                    <div className="contact-data">
                         <div className="data-item">
-                       <h2><i className="fas fa-map-marker-alt"></i></h2>
-                        <p>{SideData[this.props.language].Contact.info1} <br /> {SideData[this.props.language].Contact.info2}</p>
+                            <h2><i className="fas fa-map-marker-alt"></i></h2>
+                            <p>{SideData[this.props.language].Contact.info1} <br /> {SideData[this.props.language].Contact.info2}</p>
                         </div>
                         <div className="data-item">
-                        <h2><i className="fas fa-envelope"></i></h2>
-                        <p>{SideData[this.props.language].Contact.info3}</p>
+                            <h2><i className="fas fa-envelope"></i></h2>
+                            <p>{SideData[this.props.language].Contact.info3}</p>
                         </div>
                         <div className="data-item">
-                       <h2><i className="fas fa-phone"></i></h2>
-                        <p>{SideData[this.props.language].Contact.info4}</p>
+                            <h2><i className="fas fa-phone"></i></h2>
+                            <p>{SideData[this.props.language].Contact.info4}</p>
                         </div>
                         <div className="data-item">
-                      <h2><i className="fas fa-clock"></i></h2>
-                        <p>{SideData[this.props.language].Contact.info5} <br />{SideData[this.props.language].Contact.info6} <br /> {SideData[this.props.language].Contact.info7} </p>
+                            <h2><i className="fas fa-clock"></i></h2>
+                            <p>{SideData[this.props.language].Contact.info5} <br />{SideData[this.props.language].Contact.info6} <br /> {SideData[this.props.language].Contact.info7} </p>
                         </div>
+                        <FacebookProvider appId="123456789">
+                            <Page href="https://www.facebook.com/Kravelli-1728446920747113/" tabs="timeline" />
+                        </FacebookProvider>
                     </div>
                     <div id="container">
                         <LeafletMap center={position} zoom={this.state.zoom}>
